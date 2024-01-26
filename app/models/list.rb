c3c20de27d7510d88bb4bbe91f5f7adc26247e76
Thibault Colomb <thibault.colomb934@gmail.com>
2024-01-26 16:39:05 +0100

    added reviews

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,4 +3,6 @@ class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks, dependent: :destroy
   validates :name, presence: true, uniqueness: true
+
+  has_many :reviews, dependent: :destroy
 end
